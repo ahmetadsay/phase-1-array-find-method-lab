961bc99f887156805a5ba49b87d35aaf0dd10ac1
ahmet <adsayahmet94@hotmail.com>
2023-04-22 10:23:09 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+
+
+// function superbowlWin(element) {
+//     if ( element.result === 'W') {
+//         return element.year
+//     }
+//     return false;
+// }
+
+function superbowlWin(element) {
+    const winningRecord = element.find( item => item.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
